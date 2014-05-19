176e5e0cbde24e8eb92aab8319ab5ca5d57d447a
AndyLampert <lamperta@gmail.com>
2014-05-19 16:00:49 -0600

    clicking buttons adds item title to wishlist

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 $(function(){
 	var productTemplate = $('#product-template').html();
-	var template = Handlebars.compile(productTemplate);
+	var renderProduct = Handlebars.compile(productTemplate);
 	for (var i =0; i<productsData.productsList.length; i++){
 		// console.log((productsData.productsList[i]));
 		// param is the view data - context is the object we're going to render
-		var projectDetails = template(productsData.productsList[i])
+		var projectDetails = renderProduct(productsData.productsList[i])
 		$('#product-container').append(projectDetails);
 	};
+	var wishlistTemplate = $('#wishlist-template').html();
+	var renderWishlist = Handlebars.compile(wishlistTemplate);
+	var renderedWishlist = renderWishlist(productsData.productsList[0]);
+	$('#wishlist').append(renderedWishlist);
+
+	$('.add-to-wishlist').click(function(){
+		var productTitle = $(this).closest('.product').find('.title').text();
+		console.log(productTitle);
+		var renderedWishlist = renderWishlist({title:productTitle});
+		$('#wishlist').append(renderedWishlist);
+	});
 });
+
+
